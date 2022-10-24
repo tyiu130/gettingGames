d44d54f48812117d4a66cd47dd561d559d69b541
Kirsten <kirstenmhattori@gmail.com>
2022-10-24 16:53:37 -0600

    added more genre options. finished api calling fucntions. still to complete error handling

--- app.js
+++ app.js
@@ -1,6 +1,26 @@
 // Create an app object (gettingGames)
 const gettingGames = {};
 
+// Api call method for doing more digging
+// gettingGames.genreUrl = new URL('https://api.rawg.io/api/genres');
+// gettingGames.getGameGenres = function () {
+//   gettingGames.genreUrl.search = new URLSearchParams({
+//     key: gettingGames.rawgApiKey
+//   });
+
+//   fetch(gettingGames.genreUrl)
+//     .then(function (res) {
+//       return res.json()
+//     })
+//     .then(data => {
+//       let results = data.results
+//       console.log(results);
+//       gettingGames.displayGames(results)
+//     });
+// }
+
+
+
 // Initialize preset data in the dedicated properties
 gettingGames.url = new URL('http://api.rawg.io/api/games');
 gettingGames.rawgApiKey = 'ee7f1b60d0424455b0c8ac0b09a03e17';
@@ -12,6 +32,7 @@ gettingGames.userQuery = '';
 gettingGames.init = function(){
   gettingGames.getGames();
   gettingGames.setUpEventListeners();
+  gettingGames.getGameGenres();
 };
 
 // Create a method (getGames) to make API call on page load (no filtering by user inputs)
@@ -63,17 +84,32 @@ gettingGames.displayGames = function(arrayOfGames){
 }
 
 
-// Create a method (getUserQuery) to update the variable (userQuery) based on user input from the dropdown(in a form)
-
-
-// Create a method (getGamesByGenre) to make API calls, which takes the user input as a parameter (userQuery)
-
+// Create a method (getUserQuery) to update the variable (userQuery) based on user input from the dropdown(in a form) - event listeners
 gettingGames.setUpEventListeners = function(){
-
-  document.querySelector('#genre').addEventListener('change', function () {
+  const userQuery = document.querySelector('#genre');
+  userQuery.addEventListener('change', function (event) {
+    event.preventDefault();
     const getGamesByGenre = this.value;
-    gettingGames.getGames(getGamesByGenre);
+    gettingGames.getGamesWithFilters(getGamesByGenre);
   })
+}
+
+// Create a method (getGamesByGenre) to make API calls, which takes the user input as a parameter (userQuery) - receives arguments of userQueries when called in the event listeners 
+gettingGames.getGamesWithFilters = function (userGenre) {
+  gettingGames.url.search = new URLSearchParams({
+    key: gettingGames.rawgApiKey,
+    genres: userGenre
+  });
+
+  fetch(gettingGames.url)
+    .then(function (res) {
+      return res.json()
+    })
+    .then(data => {
+      let results = data.results
+      // console.log(results);
+      gettingGames.displayGames(results)
+    });
 }
 
 // When the API call is successful, display the result by appending the data to the results div
@@ -86,3 +122,6 @@ gettingGames.init();
   // Popup modal button for instructions, the user can click on to open for the instructions on how to use the app
 
   // expand the form containing the dropdown to have more inputs and a submit button that triggers the api call 
+
+  // see more than 20 games 
+
